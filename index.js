08003f2fb7f21289bcf785cb2417147cdcfbb610
leslie-23 <leslieajayi27@gmail.com>
2025-05-23 02:12:36 +0000

    test fix --local working

--- index.js
+++ index.js
@@ -25,13 +25,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // MongoDB connection
 mongoose
-  .connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/emailSubscriptions",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGODB_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
